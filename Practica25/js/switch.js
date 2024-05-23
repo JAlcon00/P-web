15d144d8fab435d3eea4f92a3b767fe4b201260c
JAlcon00 <c.jesusalmanza@gmail.com>
2024-05-23 09:25:24 -0600

    ningun cambio

--- Practica25/js/switch.js
+++ Practica25/js/switch.js
@@ -2,4 +2,13 @@ function toggleDarkMode() {
     const body = document.body;
     body.classList.toggle('dark-mode');
     body.classList.toggle('light-mode');
+    const icon = document.getElementById('darkModeIcon');
+    if (body.classList.contains('dark-mode')) {
+        icon.classList.remove('bi-moon-stars');
+        icon.classList.add('bi-brightness-high');
+    } else {
+        icon.classList.remove('bi-brightness-high');
+        icon.classList.add('bi-moon-stars');
+    }
+
 }
